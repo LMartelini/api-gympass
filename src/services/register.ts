deebdf73f6132a8f5ba0049f48ef599bde1e3127
LMartelini <laismartelini03@gmail.com>
2026-06-01 19:12:52 -0300

    feat: create repository pattern

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma"
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
 import { hash } from 'bcryptjs'
 
 interface RegisterServiceRequest {
@@ -12,6 +13,7 @@ export async function registerService({
     email,
     password,
 }: RegisterServiceRequest) {
+
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await prisma.user.findUnique({
@@ -24,11 +26,11 @@ export async function registerService({
         throw new Error('E-mail already exists.')
     }
 
-    await prisma.user.create({
-        data: {
-            name, 
-            email,
-            password_hash,
-        }
+    const prismaUsersRepository = new PrismaUsersRepository()
+
+    await prismaUsersRepository.create({
+        name,
+        email,
+        password_hash
     })
 }
